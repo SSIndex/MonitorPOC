0deeed1bf3efe1bc0a94370755c2a0137f47cb11
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-03-03 17:39:06 -0300

    fix(_components/datepicker): MODIFY datepicker style

--- monitor/src/_components/datepicker.tsx
+++ monitor/src/_components/datepicker.tsx
@@ -1,10 +1,13 @@
 // Dummy DatePicker Component (Placeholder)
 export default function DatePickerYearly() {
     return (
-      <select className="mt-2 p-2 border rounded-md text-gray-700">
-        <option>2025</option>
-        <option>2024</option>
-        <option>2023</option>
+    <div className="flex items-center space-x-4 mt-4">
+        <p>Timeframe:</p>
+      <select className="mt-2 p-2 border rounded-md text-gray-700 bg-white">
+        <option>Dec 2025</option>
+        <option>Dec 2024</option>
+        <option>Dec 2023</option>
       </select>
+    </div>
     );
   }
