0ec7a1713453fa31e6bdfe6dd82e9356e33b9753
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-03-03 12:25:22 -0300

    feat(next.config.ts): ADD elastic beanstalk for image domain

--- monitor/next.config.ts
+++ monitor/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['elasticbeanstalk-us-east-1-518344696083.s3.us-east-1.amazonaws.com']
+  }
 };
 
 export default nextConfig;
