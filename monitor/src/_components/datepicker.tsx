2b2ddf590446653081043febe6e9a116c76c16ec
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-03-04 16:27:00 -0300

    refactor(_components/datepicker): IMPROVE design

--- monitor/src/_components/datepicker.tsx
+++ monitor/src/_components/datepicker.tsx
@@ -4,9 +4,9 @@ export default function DatePickerYearly() {
     <div className="flex items-center space-x-4 mt-4">
         <p>Timeframe:</p>
       <select className="mt-2 p-2 border rounded-md text-gray-700 bg-white">
-        <option>Dec 2025</option>
-        <option>Dec 2024</option>
-        <option>Dec 2023</option>
+        <option>Dec 2025 - Dec 2024</option>
+        <option>Dec 2024 - Dec 2023</option>
+        <option>Dec 2023 - Dec 2022</option>
       </select>
     </div>
     );
