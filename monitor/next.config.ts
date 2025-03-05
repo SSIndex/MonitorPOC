a0e318055405a8f4b1b36b0c311373fc057127c7
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-03-05 14:51:46 -0300

    fix(next.config.ts): UPGRADE to image domain to remotePatterns

--- monitor/next.config.ts
+++ monitor/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['elasticbeanstalk-us-east-1-518344696083.s3.us-east-1.amazonaws.com']
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'elasticbeanstalk-us-east-1-518344696083.s3.us-east-1.amazonaws.com',
+      port: '',
+      search: '',
+    }]
   }
 };
 
