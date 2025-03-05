4b1eb156c5b10687f67dee08721e24e06ecfafe8
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-03-05 15:49:05 -0300

    style(_components/table): APPLY formatting to code

--- monitor/src/_components/table.tsx
+++ monitor/src/_components/table.tsx
@@ -184,243 +184,245 @@ export function Table() {
   });
 
   const totalScore = Math.round(
-    dimensionData.reduce((sum, row) => sum + row.score, 0) / dimensionData.length
+    dimensionData.reduce((sum, row) => sum + row.score, 0) /
+      dimensionData.length
   );
   const totalPercentile = Math.round(
-    dimensionData.reduce((sum, row) => sum + row.percentile, 0) / dimensionData.length
+    dimensionData.reduce((sum, row) => sum + row.percentile, 0) /
+      dimensionData.length
   );
 
   return (
-    <div>
-      <table className="table-fixed w-full border-collapse">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header, i) => {
-                if (i === 0) {
-                  return (
-                    <th
-                      key={header.id}
-                      className="bg-ssindex-table-header-gray text-primary rounded-l-lg w-10"
-                      style={{ padding: "8px", textAlign: "center" }}
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : header.column.columnDef.header}
-                    </th>
-                  );
-                }
+    <table className="table-fixed w-full border-collapse">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header, i) => {
+              if (i === 0) {
+                return (
+                  <th
+                    key={header.id}
+                    className="bg-ssindex-table-header-gray text-primary rounded-l-lg w-10"
+                    style={{ padding: "8px", textAlign: "center" }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : header.column.columnDef.header}
+                  </th>
+                );
+              }
 
-                if (i === 1) {
-                  return (
-                    <th
-                      key={header.id}
-                      className="bg-ssindex-table-header-gray text-primary w-50"
-                      style={{ padding: "8px", textAlign: "left" }}
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : header.column.columnDef.header}
-                    </th>
-                  );
-                } else if (i === headerGroup.headers.length - 1) {
-                  return (
-                    <th
-                      key={header.id}
-                      className="bg-ssindex-table-header-gray text-primary rounded-r-lg"
-                      style={{ padding: "8px", textAlign: "center" }}
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : header.column.columnDef.header}
-                    </th>
-                  );
-                } else {
-                  return (
-                    <th
-                      key={header.id}
-                      className="bg-ssindex-table-header-gray text-primary"
-                      style={{ padding: "8px", textAlign: "center" }}
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : header.column.columnDef.header}
-                    </th>
-                  );
-                }
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <React.Fragment key={row.id}>
-              <tr
-                onClick={row.getToggleExpandedHandler()}
-                className={`hover:bg-neutral-200 ${row.getIsExpanded() ? "bg-neutral-200" : "bg-white"}`}
-                // style={{ cursor: 'pointer', background: row.getIsExpanded() ? '#f0f0f0' : 'white' }}
-              >
-                {row.getVisibleCells().map((cell, i) => {
-                  console.log("cell", cell);
-                  console.log("cell.column.id", cell.column.id);
-                  const columnId = cell.column.id;
+              if (i === 1) {
+                return (
+                  <th
+                    key={header.id}
+                    className="bg-ssindex-table-header-gray text-primary w-50"
+                    style={{ padding: "8px", textAlign: "left" }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : header.column.columnDef.header}
+                  </th>
+                );
+              } else if (i === headerGroup.headers.length - 1) {
+                return (
+                  <th
+                    key={header.id}
+                    className="bg-ssindex-table-header-gray text-primary rounded-r-lg"
+                    style={{ padding: "8px", textAlign: "center" }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : header.column.columnDef.header}
+                  </th>
+                );
+              } else {
+                return (
+                  <th
+                    key={header.id}
+                    className="bg-ssindex-table-header-gray text-primary"
+                    style={{ padding: "8px", textAlign: "center" }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : header.column.columnDef.header}
+                  </th>
+                );
+              }
+            })}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <React.Fragment key={row.id}>
+            <tr
+              onClick={row.getToggleExpandedHandler()}
+              className={`hover:bg-neutral-200 ${row.getIsExpanded() ? "bg-neutral-200" : "bg-white"}`}
+              // style={{ cursor: 'pointer', background: row.getIsExpanded() ? '#f0f0f0' : 'white' }}
+            >
+              {row.getVisibleCells().map((cell, i) => {
+                console.log("cell", cell);
+                console.log("cell.column.id", cell.column.id);
+                const columnId = cell.column.id;
 
-                  const value = cell.getValue();
-                  const commonClasses =
-                    "pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center";
-                  let backgroundColor = "";
-                  // if value is number, categorize it
-                  if (
-                    [
-                      "noData",
-                      "poor",
-                      "low",
-                      "average",
-                      "good",
-                      "excellent",
-                      "score",
-                      "percentile",
-                    ].includes(columnId)
-                  ) {
-                    if (columnId === "noData") {
-                      backgroundColor = "bg-ssindex-no-data";
-                    } else {
-                      backgroundColor = `${categorizeScoreToBgClassName(value)}`;
-                    }
-                  }
-                  const opacityClass = (value as boolean)
-                    ? "opacity-100"
-                    : "opacity-30";
-                  const coloredCells = [
+                const value = cell.getValue();
+                const commonClasses =
+                  "pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center";
+                let backgroundColor = "";
+                // if value is number, categorize it
+                if (
+                  [
                     "noData",
                     "poor",
                     "low",
                     "average",
                     "good",
                     "excellent",
-                  ];
-
-                  if (coloredCells.includes(columnId)) {
-                    // Determine background color based on columnId
-                    switch (columnId) {
-                      case "noData":
-                        backgroundColor = "bg-ssindex-no-data";
-                        break;
-                      case "poor":
-                        backgroundColor = "bg-ssindex-poor";
-                        break;
-                      case "low":
-                        backgroundColor = "bg-ssindex-low";
-                        break;
-                      case "average":
-                        backgroundColor = "bg-ssindex-average";
-                        break;
-                      case "good":
-                        backgroundColor = "bg-ssindex-good";
-                        break;
-                      case "excellent":
-                        backgroundColor = "bg-ssindex-excellent";
-                        break;
-                    }
+                    "score",
+                    "percentile",
+                  ].includes(columnId)
+                ) {
+                  if (columnId === "noData") {
+                    backgroundColor = "bg-ssindex-no-data";
+                  } else {
+                    backgroundColor = `${categorizeScoreToBgClassName(value)}`;
                   }
+                }
+                const opacityClass = (value as boolean)
+                  ? "opacity-100"
+                  : "opacity-30";
+                const coloredCells = [
+                  "noData",
+                  "poor",
+                  "low",
+                  "average",
+                  "good",
+                  "excellent",
+                ];
 
-                  if (coloredCells.includes(columnId)) {
+                if (coloredCells.includes(columnId)) {
+                  // Determine background color based on columnId
+                  switch (columnId) {
+                    case "noData":
+                      backgroundColor = "bg-ssindex-no-data";
+                      break;
+                    case "poor":
+                      backgroundColor = "bg-ssindex-poor";
+                      break;
+                    case "low":
+                      backgroundColor = "bg-ssindex-low";
+                      break;
+                    case "average":
+                      backgroundColor = "bg-ssindex-average";
+                      break;
+                    case "good":
+                      backgroundColor = "bg-ssindex-good";
+                      break;
+                    case "excellent":
+                      backgroundColor = "bg-ssindex-excellent";
+                      break;
+                  }
+                }
+
+                if (coloredCells.includes(columnId)) {
+                  return (
+                    <td
+                      key={cell.id}
+                      className={`${commonClasses} ${i === 1 ? "text-left" : "text-center"}`}
+                    >
+                      <div
+                        className={`${backgroundColor} ${opacityClass} flex align-items-center border-3 border-dark  w-full h-full`}
+                      >
+                        {"\u200B"}
+                      </div>
+                    </td>
+                  );
+                }
+
+                switch (columnId) {
+                  case "score":
                     return (
                       <td
                         key={cell.id}
-                        className={`${commonClasses} ${i === 1 ? "text-left" : "text-center"}`}
+                        className={`${commonClasses} text-white font-bold ${i === 1 ? "text-left" : "text-center"}`}
                       >
                         <div
-                          className={`${backgroundColor} ${opacityClass} flex align-items-center border-3 border-dark  w-full h-full`}
+                          className={`${backgroundColor} rounded-sm w-full h-full flex items-center justify-center`}
                         >
-                          {"\u200B"}
+                          {`${value}%`}
                         </div>
                       </td>
                     );
-                  }
-
-                  switch (columnId) {
-                    case "score":
-                      return (
-                        <td
-                          key={cell.id}
-                          className={`${commonClasses} text-white font-bold ${i === 1 ? "text-left" : "text-center"}`}
+                  case "percentile":
+                    return (
+                      <td
+                        key={cell.id}
+                        className={`${commonClasses} text-white font-bold ${i === 1 ? "text-left" : "text-center"}`}
+                      >
+                        <div
+                          className={`${backgroundColor} rounded-sm w-full h-full flex items-center justify-center`}
                         >
-                          <div
-                            className={`${backgroundColor} rounded-sm w-full h-full flex items-center justify-center`}
-                          >
-                            {`${value}%`}
-                          </div>
-                        </td>
-                      );
-                    case "percentile":
-                      return (
-                        <td
-                          key={cell.id}
-                          className={`${commonClasses} text-white font-bold ${i === 1 ? "text-left" : "text-center"}`}
-                        >
-                          <div
-                            className={`${backgroundColor} rounded-sm w-full h-full flex items-center justify-center`}
-                          >
-                            {`${value}th`}
-                          </div>
-                        </td>
-                      );
-                    default:
-                      return (
-                        <td
-                          key={cell.id}
-                          className={`${commonClasses} text-primary ${i === 1 ? "text-left" : "text-center"}`}
-                        >
-                          <div className={`${backgroundColor} w-full h-full `}>
-                            {value}
-                          </div>
-                        </td>
-                      );
-                  }
-                })}
+                          {`${value}th`}
+                        </div>
+                      </td>
+                    );
+                  default:
+                    return (
+                      <td
+                        key={cell.id}
+                        className={`${commonClasses} text-primary ${i === 1 ? "text-left" : "text-center"}`}
+                      >
+                        <div className={`${backgroundColor} w-full h-full `}>
+                          {value}
+                        </div>
+                      </td>
+                    );
+                }
+              })}
+            </tr>
+            {row.getIsExpanded() && (
+              <tr>
+                <td
+                  colSpan={row.getVisibleCells().length}
+                  style={{ padding: "0" }}
+                >
+                  <NestedOrderTable orders={row.original.orders} />
+                </td>
               </tr>
-              {row.getIsExpanded() && (
-                <tr>
-                  <td
-                    colSpan={row.getVisibleCells().length}
-                    style={{ padding: "0" }}
-                  >
-                    <NestedOrderTable orders={row.original.orders} />
-                  </td>
-                </tr>
-              )}
-            </React.Fragment>
-          ))}
+            )}
+          </React.Fragment>
+        ))}
 
-          {/* Footer Row */}
-          <tr className="border-t-1 font-bold border-gray-300">
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-left">Total Score</td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center">
-              <div
-                className={`${categorizeScoreToBgClassName(totalScore)} rounded-sm w-full h-full flex items-center justify-center text-white`}
-              >
-                {`${totalScore}%`}
-              </div>
-            </td>
-            <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center">
-              <div
-                className={`${categorizeScoreToBgClassName(totalPercentile)} rounded-sm w-full h-full flex items-center justify-center text-white`}
-              >
-                {`${totalPercentile}th`}
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+        {/* Footer Row */}
+        <tr className="border-t-1 font-bold border-gray-300">
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-left">
+            Total Score
+          </td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center"></td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center">
+            <div
+              className={`${categorizeScoreToBgClassName(totalScore)} rounded-sm w-full h-full flex items-center justify-center text-white`}
+            >
+              {`${totalScore}%`}
+            </div>
+          </td>
+          <td className="pt-5 pb-5 ps-1 pe-1 text-primary h-20 text-center">
+            <div
+              className={`${categorizeScoreToBgClassName(totalPercentile)} rounded-sm w-full h-full flex items-center justify-center text-white`}
+            >
+              {`${totalPercentile}th`}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
