7e52ba23cd327f8ac9ef06dba02502b3884cc365
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-03-11 14:12:14 -0300

    fix(requests/sasb): MODIFY endpoint

--- monitor/src/_handlers/requests/sasb.ts
+++ monitor/src/_handlers/requests/sasb.ts
@@ -4,7 +4,7 @@ export const useGetOverallScoreSASB = () => {
   return useQuery({
     queryKey: ["overallScoreSASB"],
     queryFn: async () => {
-      const response = await fetch("/api/v1/sasb");
+      const response = await fetch("/api/v1/sasb/overall");
       return response.json();
     },
   });
