4a2c0f30d84de4e4e89e29c169dc3653e70f4956
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-03-12 11:08:24 -0300

    feat(_handlers/requests/sasb): ADD sasb reviews handler

--- monitor/src/_handlers/requests/sasb.ts
+++ monitor/src/_handlers/requests/sasb.ts
@@ -1,10 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 
+const baseURL = "/api/v1/sasb";
+
 export const useGetOverallScoreSASB = () => {
   return useQuery({
     queryKey: ["overallScoreSASB"],
     queryFn: async () => {
-      const response = await fetch("/api/v1/sasb/overall");
+      const response = await fetch(`${baseURL}/overall`);
+      return response.json();
+    },
+  });
+};
+
+export const useGetSASBReviews = (dimension: string, companyName: string) => {
+  let endpoint = `${baseURL}/reviews`;
+  endpoint =
+    dimension && companyName
+      ? `${endpoint}?dimension=${dimension}&companyName=${companyName}`
+      : endpoint;
+
+  return useQuery({
+    queryKey: ["sasbReviews", dimension, companyName],
+    queryFn: async () => {
+      const response = await fetch(endpoint);
       return response.json();
     },
   });
